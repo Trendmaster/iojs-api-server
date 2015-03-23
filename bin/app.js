3d381c781c0c0fd7f1362b53a449a9992fbf3f9f
bsdo <bsdo@naver.com>
2015-03-24 01:15:02 +0900

    Logger 추가

--- bin/app.js
+++ bin/app.js
@@ -2,13 +2,38 @@
  * Created by dobyeongsu on 15. 3. 20..
  */
 var restify = require('restify');
+var Logger = require('bunyan');
 
-var server = restify.createServer({
-  name: 'MyApp'
+var log = new Logger({
+  name: 'restifypretest',
+  streams: [{
+    path: '/Users/dobyeongsu/Desktop/log.log',
+    type: 'rotating-file',
+    period: '1d',   // daily rotation
+    count: 3        // keep 3 back copies
+  }],
+  serializers: {
+    req: Logger.stdSerializers.req,
+    res: Logger.stdSerializers.res,
+    err: Logger.stdSerializers.err
+  }
 });
 
-server.use(function (req, res, next) {
+var server = restify.createServer({
+  name: 'restifypretest',
+  log: log
+});
+
+server.pre(function(req, res, next) {
+  server.log.info({
+    req : req,
+    kr_time: Date("Y-m-d H:i:s")
+  }, 'firstRequest');
   next();
+});
+
+server.on('after', function (req, res) {
+  server.log.info({res: res}, "finished");
 });
 
 require('../lib')(server);
